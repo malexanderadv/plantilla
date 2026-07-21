d89d305438c81123e30777be58ca08c09e809607
malexander <malexanderadv@gmail.com>
2026-07-21 12:00:45 -0500

    plantilla #1 completa

---
+++ src/main/java/persistencia/EjUsuarioJpaController.java
@@ -0,0 +1,137 @@
+
+package persistencia;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import java.io.Serializable;
+import jakarta.persistence.Query;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import java.util.List;
+import logica.Usuarios;
+import persistencia.exceptions.NonexistentEntityException;
+
+
+public class UsuariosJpaController implements Serializable {
+
+    public UsuariosJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    
+    public UsuariosJpaController() {
+        this.emf = Persistence.createEntityManagerFactory("PruebaJavaWeb");
+    }
+    
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Usuarios usuarios) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(usuarios);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Usuarios usuarios) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            usuarios = em.merge(usuarios);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                int id = usuarios.getId();
+                if (findUsuarios(id) == null) {
+                    throw new NonexistentEntityException("The usuarios with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(int id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Usuarios usuarios;
+            try {
+                usuarios = em.getReference(Usuarios.class, id);
+                usuarios.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The usuarios with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(usuarios);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Usuarios> findUsuariosEntities() {
+        return findUsuariosEntities(true, -1, -1);
+    }
+
+    public List<Usuarios> findUsuariosEntities(int maxResults, int firstResult) {
+        return findUsuariosEntities(false, maxResults, firstResult);
+    }
+
+    private List<Usuarios> findUsuariosEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Usuarios.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Usuarios findUsuarios(int id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Usuarios.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getUsuariosCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Usuarios> rt = cq.from(Usuarios.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
